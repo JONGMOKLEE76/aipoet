f5715d8af188865650d078bc27d62555b0dc117a
Pablo <gilbut76@gmail.com>
2024-01-06 19:16:15 +0900

    lama version

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI, CTransformers
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
+
 
 # llm = OpenAI()
 # result = llm.predict('모니터 아웃소싱을 하는데 있어서 가장 핵심적인 일')
 
-chat_model = ChatOpenAI()
+# chat_model = ChatOpenAI()
+llm = CTransformers(
+    model='llama-2-7b-chat.ggmlv3.q2_K.bin',
+    model_type='llama'
+)
 
 st.title('인공지능 시인')
 
@@ -17,5 +22,5 @@ content = st.text_input('시의 주제를 제시해주세요.')
 
 if st.button('시 작성 요청하기'):
     with st.spinner('시 작성 중...'):
-        result = chat_model.predict(content + '에 대한 시를 써줘')
+        result = llm.predict('Write a poem about ' + content)
         st.write(result)
